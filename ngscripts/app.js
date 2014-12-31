806970b403456023652b32b88b521e5d1d2e184c
kristdev (Franck Pambe) <info@kristdev.com>
2014-12-31 21:43:04 +0100

    311220142142
    Intégration première directive angularjs

--- ngscripts/app.js
+++ ngscripts/app.js
@@ -6,66 +6,11 @@
 	*/
 	var app=angular.module('beth', []);
 
-	app.directive('headerTop', ['headerTop', function(){
-		// Runs during compile
-		return {
-			// name: '',
-			// priority: 1,
-			// terminal: true,
-			// scope: {}, // {} = isolate, true = child, false/undefined = no change
-			// controller: function($scope, $element, $attrs, $transclude) {},
-			// require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
-			restrict: 'E', // E = Element, A = Attribute, C = Class, M = Comment
-			// template: '',
-			templateUrl: '../includes/heder-top.html',
-			// replace: true,
-			// transclude: true,
-			// compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
-			/*link: function($scope, iElm, iAttrs, controller) {
-				
-			}*/
-		};
-	}]);
+	app.directive("headertop", function(){
+		return{
+			restrict: 'E',
+			templateUrl: 'includes/headertop.html'
+		}
+	});
 
-	app.directive('', ['', function(){
-		// Runs during compile
-		return {
-			// name: '',
-			// priority: 1,
-			// terminal: true,
-			// scope: {}, // {} = isolate, true = child, false/undefined = no change
-			// controller: function($scope, $element, $attrs, $transclude) {},
-			// require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
-			// restrict: 'A', // E = Element, A = Attribute, C = Class, M = Comment
-			// template: '',
-			// templateUrl: '',
-			// replace: true,
-			// transclude: true,
-			// compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
-			/*link: function($scope, iElm, iAttrs, controller) {
-				
-			}*/
-		};
-	}]);
-
-	app.directive('', ['', function(){
-		// Runs during compile
-		return {
-			// name: '',
-			// priority: 1,
-			// terminal: true,
-			// scope: {}, // {} = isolate, true = child, false/undefined = no change
-			// controller: function($scope, $element, $attrs, $transclude) {},
-			// require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
-			// restrict: 'A', // E = Element, A = Attribute, C = Class, M = Comment
-			// template: '',
-			// templateUrl: '',
-			// replace: true,
-			// transclude: true,
-			// compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
-			link: function($scope, iElm, iAttrs, controller) {
-				
-			}
-		};
-	}]);
 })();
